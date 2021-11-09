4e8830f6f79512b2fa2d384f8cb240f2bf16a8e7
filip19992 <filipwlodarczyk99@gmail.com>
2021-11-09 11:19:36 +0100

    adding AuthorizationFilter and some clean up

--- src/main/java/pl/filipwlodarczyk/SpringSecurity/security/SecurityConfig.java
+++ src/main/java/pl/filipwlodarczyk/SpringSecurity/security/SecurityConfig.java
@@ -12,8 +12,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pl.filipwlodarczyk.SpringSecurity.filter.AuthorizationFilter;
 import pl.filipwlodarczyk.SpringSecurity.filter.CustomAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -22,7 +26,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
-//    private final BCryptPasswordEncoder passwordEncoder;
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -39,8 +43,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/login", "/api/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers("/api/user/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/api/users").
+                hasAnyAuthority("ROLE_ADMIN", "ROLE_MANAGER", "ROLE_USER");
+        http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManager()));
+        http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
